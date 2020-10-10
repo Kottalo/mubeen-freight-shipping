1db691bf2a3ff3eae53be51d84a80c32986b2359
Kottalo <kottaloflotenspieler@gmail.com>
2020-10-10 22:15:44 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
+    
+    <title>UNIEX</title>
+  </head>
+  <body>
+    <div id="app">
+      <app></app>
+    </div>
+    <script src="js/app.js">
+
+    </script>
+  </body>
+</html>
